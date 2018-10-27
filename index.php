db5a3dd118a8f81a33cb795a660cae94b8822dac
Dario Tislar <dario.tislar@gmail.com>
2018-10-27 21:44:52 +0200

    initial push

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Otvorrrrr vrata</title>
+    </head>
+ 
+    <body style="background-color: black;">
+    <!-- On/Off button's picture -->
+	<div id="wrapper" style="width:100%; text-align:center">
+	<?php
+	$ctl_gpio_pin = 7;
+	$gpio_desired_default = 1;
+	//this php script generate the first page in function of the file
+	system("gpio write ".$ctl_gpio_pin." ".$gpio_desired_default);
+	system("gpio mode ".$ctl_gpio_pin." out");
+	exec ("gpio read ".$ctl_gpio_pin, $ctl_gpio_state, $return );
+	
+	if ($ctl_gpio_state == 0 ) {
+		echo ("<img id='button' src='data/img/red/red.jpg' onclick='change_pin (".$ctl_gpio_pin.");'/>");
+	}
+	else
+	{
+		echo ("<img id='button' src='data/img/green/green.jpg' onclick='change_pin (".$ctl_gpio_pin.");'/>");
+	}	
+	?>
+	 </div>
+	<!-- javascript -->
+	<script src="script.js"></script>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,51 @@
+﻿var button = document.getElementById("button");
+var red_btn = "data/img/red/red.jpg";
+var green_btn = "data/img/green/green.jpg";
+
+function change_pin(gpio_pin) {
+	if (!button.src.includes(green_btn)) {
+		console.log("Ne pritisci kad sam crven!");
+		return;
+	}
+	if (confirm('Sigurno ?')) {		
+		button.src = red_btn;		
+		send_req(gpio_pin);
+	}
+
+	return 0;
+}
+
+function send_req(gpio_pin) {
+	var request = new XMLHttpRequest();
+	request.open("GET", "gpio.php?pic=" + gpio_pin, true);
+	request.send(null);
+	//receiving informations
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			data = request.responseText;
+			//update the index pic
+			if ( data == "1" || data == "0") {
+				document.body.style.background = 'gray';				
+				button = green_btn;
+			}
+			else if (!(data.localeCompare("fail"))) {
+				alert("Something went wrong!");
+				return ("fail");
+			}
+			else {
+				alert("Something went wrong!");
+				return ("fail");
+			}
+		}
+		//test if fail
+		else if (request.readyState == 4 && request.status == 500) {
+			alert("server error");
+			return ("fail");
+		}
+		//else 
+		else if (request.readyState == 4 && request.status != 200 && request.status != 500) {
+			alert("Something went wrong!");
+			return ("fail");
+		}
+	}
+}
